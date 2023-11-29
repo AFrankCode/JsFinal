8eef8284b46f909da0946a9988b6b7e9844c3d55
AFrankCode <139520217+AFrankCode@users.noreply.github.com>
2023-11-28 20:05:24 -0600

    3rd problem solved

---
+++ problem3.js
@@ -0,0 +1,10 @@
+function numOddValues(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]%2 !== 0) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(numOddValues([]))
